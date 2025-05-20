48e2b2ebdd5f6928862cd3ce82ba77da784237a4
Manasi Kale <miss.manasikale@gmail.com>
2025-05-20 13:10:54 +0530

    deployment Changes

--- Website/src/main.jsx
+++ Website/src/main.jsx
@@ -4,7 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://api.toyshop.sbs/api';
+axios.defaults.baseURL = 'http://69.62.81.214:3000/api';
+// axios.defaults.baseURL = 'https://api.toyshop.sbs/api';
 // axios.defaults.baseURL = 'http://localhost:3000/api';
 
 createRoot(document.getElementById('root')).render(
