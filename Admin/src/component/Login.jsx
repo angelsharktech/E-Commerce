639c4bbb03a7bec4384c66188bf4ed30b6a97211
Manasi Kale <miss.manasikale@gmail.com>
2025-05-27 11:03:41 +0530

    login css

--- Admin/src/component/Login.jsx
+++ Admin/src/component/Login.jsx
@@ -1,7 +1,7 @@
 
 import { Box, Button, Grid, TextField } from '@mui/material'
 import React, { useContext, useState } from 'react'
-import './login.css'
+import './Login.css'
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios'
 import { userInformation } from '../context/AuthContext';
