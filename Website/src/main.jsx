0a06174a3ac8179b959c0769a899f46eb3af1654
Manasi Kale <160154891+manukale@users.noreply.github.com>
2025-06-02 16:52:45 +0530

    Update main.jsx

--- Website/src/main.jsx
+++ Website/src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://api.toyshop.sbs/api';
-// axios.defaults.baseURL = 'http://69.62.81.214:3000/api';
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
