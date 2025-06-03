05b3c8665e4bcbc33ec82430406845e0a4299f7a
Manasi Kale <miss.manasikale@gmail.com>
2025-06-03 16:13:02 +0530

    corsOption added

--- API/app.js
+++ API/app.js
@@ -15,19 +15,18 @@ const port = process.env.PORT || 3000;
 
 const allowedOrigins = ['https://toyshop.sbs', 'https://admin.toyshop.sbs'];
 
-app.use(
-    cors({
+const corsOptions = {
       origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, origin); // return the specific origin string
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+          callback(null, true); // return the specific origin string
         } else {
           callback(new Error('Not allowed by CORS'));
         }
       },
       credentials: true,
       methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-    })
-  );
+    }
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
