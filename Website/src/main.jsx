2b6ba8bf51bad47fa46ebfafea1bfde0a2840651
Manasi Kale <miss.manasikale@gmail.com>
2025-07-07 10:34:35 +0530

    Update main.jsx for window scrolling issue

--- Website/src/main.jsx
+++ Website/src/main.jsx
@@ -8,7 +8,7 @@ axios.defaults.baseURL = 'https://api.starbasket.in/api';
 // axios.defaults.baseURL = 'https://api.toyshop.sbs/api';
 
 
-
+window.history.scrollRestoration = 'manual';
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
